24958b2b4fa89868538ea6e5142351da78398e8d
Toby Fang <tobyfang05@gmail.com>
2025-05-15 19:04:06 +0800

    request and responses

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
-const PORT = 6000
+const PORT = 3000
 
+app.get('/', (req, res) => {
+    console.log("Hit endpoint", req.method);
+    res.sendStatus(200)
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log("Dashbaord")
+    res.send("Hi")
+})
 
 app.listen(PORT, () => console.log(`Server started on: ${PORT}`))
