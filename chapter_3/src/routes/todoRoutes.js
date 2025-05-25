eea106c9f9ac1c80a45e60fc8d96b3946316424a
Toby Fang <tobyfang05@gmail.com>
2025-05-25 18:57:02 +0800

    todo app complete

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -11,17 +11,33 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    const { task } = req.body;
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`);
 
+    const result = insertTodo.run(req.userId, task);
+
+    res.json({id: result.lastID, task, completed: 0});
 });
 
 //update a todo
 router.put('/:id', (req, res) => {
+    const {completed} = req.body;
+    const {id} = req.params;
+    
+    const updateTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`);
+    updateTodo.run(completed, id);
 
+    res.json({'message': "todo complete"});
 });
 
 //delete a todo
 router.delete('/:id', (req,res) => {
+    const {id} = req.params;
+    const userId = req.userId;
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`);
+    deleteTodo.run(id, userId);
 
+    res.json({'id': id});
 });
 
 export default router;
